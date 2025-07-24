c526f884dff353cd140e247717660b26b76e0795
chaurasiavipin <vipinchaurasia481@gmail.com>
2025-07-24 11:28:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
+const highLight=document.querySelectorAll("strong");
+const newLight=document.querySelectorAll("p");
+
+
+
 function highlight() {
-    //Write your code here
-
-
+    highlight. style.color="rgb(0, 128, 0)";
+	
 }
 
 
 function return_normal() {
-    //Write your code here
+     highlight. style.color="rgb(0, 0, 0)";
 
-    
 }
+newLight.addEventListener('mouseover', highlight);
+newLight.addEventListener('mouseout', highlight)
+
+
