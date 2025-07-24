c7a0719ae5f8b6c7a5fb9f869df181d72ac21827
chaurasiavipin <vipinchaurasia481@gmail.com>
2025-07-24 12:19:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 const highLight=document.querySelectorAll("strong");
-const newLight=document.querySelector("p");
+
+const newLight=document.querySelectorAll("p");
+const store=highLight.length;
+const store2=newLight.length;
 
 
 
 function highlight() {
-    highLight. style.color="rgb(0, 128, 0)";
-	
+	for(let i=0; i<store; i++){
+    highLight[i].style.color= "rgb(0,128,0)";
+	}
 }
 
 
 function return_normal() {
-     highLight. style.color="rgb(0, 0, 0)";
+	for(let i=0; i<store2; i++){
+
+     highLight[i].style.color= "rgb(0, 0, 0)";
 
 }
-newLight.addEventListener('mouseover', highlight);
-newLight.addEventListener('mouseout', return_normal)
-
-
+}
